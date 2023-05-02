ee970a8d243d7cd1f0ece1792058e846bae50472
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-05-02 14:44:23 +0200

    bug-reproduction: use shared files for molecule scenario

--- molecule/encryption/tests/test_default.py
+++ molecule/encryption/tests/test_default.py
@@ -1,11 +1,1 @@
-#!/usr/bin/env python
-
-
-def test_package_is_installed(host):
-    mariadb_server_debian = host.package("mariadb-server")
-    mariadb_server_redhat = host.package("MariaDB-server")
-    assert mariadb_server_debian.is_installed or mariadb_server_redhat.is_installed
-
-
-def test_service_listening(host):
-    assert host.socket("tcp://0.0.0.0:3306").is_listening
+/home/bngameni/Desktop/Claranet/ansible-role-mariadb/molecule/shared/test_default.py
---
+++ molecule/shared/test_encryption.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+def get_mysql_config_path(host):
+    config_path = ""
+    os_release = host.file('/etc/os-release')
+    for os in ('centos', 'fedora', 'rhel','amzn'):
+        if os_release.contains(f'ID="{os}"'):
+            config_path = "/etc/my.cnf"
+            break
+        else:
+            config_path = "etc/mysql/my.cnf"
+
+    return config_path
+
+def test_encryption_keys(host):
+    encrypted_file = host.file("/etc/mysql/encryption/keys.enc")
+    encrypted_key_file = host.file("/etc/mysql/encryption/password_file")
+    assert encrypted_file.exists and encrypted_key_file.exists
+
+
+def test_encryption_is_used(host):
+    mysql_config = host.file(get_mysql_config_path(host))
+    assert mysql_config.exists
+    assert "file_key_management_filename" in mysql_config.content_string
